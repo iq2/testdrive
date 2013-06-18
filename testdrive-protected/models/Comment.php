310c5f5bcfa75b0789de33d71ea9b14773589549
Florian Bacher <florian.bacher@iq2.at>
2013-06-18 12:12:48 +0200

    correct indention

--- testdrive-protected/models/Comment.php
+++ testdrive-protected/models/Comment.php
@@ -1,111 +1,111 @@
 <?php
 
 /**
- * This is the model class for table "comment".
- *
- * The followings are the available columns in table 'comment':
- * @property integer $id
- * @property string $comment
- * @property string $date_entered
- * @property string $user_id
- * @property string $page_id1
- *
- * The followings are the available model relations:
- * @property User $user
- * @property Page $pageId1
- */
+* This is the model class for table "comment".
+*
+* The followings are the available columns in table 'comment':
+* @property integer $id
+* @property string $comment
+* @property string $date_entered
+* @property string $user_id
+* @property string $page_id1
+*
+* The followings are the available model relations:
+* @property User $user
+* @property Page $pageId1
+*/
 class Comment extends CActiveRecord
 {
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'comment';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'comment';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('comment', 'required'),
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('comment', 'required'),
             array('user_id, page_id1', 'exist'),
-			array('comment', 'filter', 'filter'=>'strip_tags'),
+            array('comment', 'filter', 'filter'=>'strip_tags'),
             array('date_entered', 'default', 'value'=>new CDbExpression('NOW()'), 'on'=>'insert'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, comment, date_entered, user_id, page_id1', 'safe', 'on'=>'search'),
-		);
-	}
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, comment, date_entered, user_id, page_id1', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
-			'pageId1' => array(self::BELONGS_TO, 'Page', 'page_id1'),
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+            'pageId1' => array(self::BELONGS_TO, 'Page', 'page_id1'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'comment' => 'Comment',
-			'date_entered' => 'Date Entered',
-			'user_id' => 'User',
-			'page_id1' => 'Page Id1',
-		);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'comment' => 'Comment',
+            'date_entered' => 'Date Entered',
+            'user_id' => 'User',
+            'page_id1' => 'Page Id1',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria=new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('comment',$this->comment,true);
-		$criteria->compare('date_entered',$this->date_entered,true);
-		$criteria->compare('user_id',$this->user_id,true);
-		$criteria->compare('page_id1',$this->page_id1,true);
+        $criteria->compare('id',$this->id);
+        $criteria->compare('comment',$this->comment,true);
+        $criteria->compare('date_entered',$this->date_entered,true);
+        $criteria->compare('user_id',$this->user_id,true);
+        $criteria->compare('page_id1',$this->page_id1,true);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Comment the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Comment the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 }
--- testdrive-protected/models/Page.php
+++ testdrive-protected/models/Page.php
@@ -1,125 +1,125 @@
 <?php
 
 /**
- * This is the model class for table "page".
- *
- * The followings are the available columns in table 'page':
- * @property string $id
- * @property integer $live
- * @property string $title
- * @property string $content
- * @property string $date_updated
- * @property string $date_published
- * @property string $user_id
- *
- * The followings are the available model relations:
- * @property Comment[] $comments
- * @property User $user
- * @property File[] $files
- */
+* This is the model class for table "page".
+*
+* The followings are the available columns in table 'page':
+* @property string $id
+* @property integer $live
+* @property string $title
+* @property string $content
+* @property string $date_updated
+* @property string $date_published
+* @property string $user_id
+*
+* The followings are the available model relations:
+* @property Comment[] $comments
+* @property User $user
+* @property File[] $files
+*/
 class Page extends CActiveRecord
 {
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'page';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'page';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('title', 'required'),
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('title', 'required'),
             array('user_id', 'exist'),
-			array('live', 'boolean'),
+            array('live', 'boolean'),
             array('live', 'default', 'value'=>0),
-			array('title', 'length', 'max'=>100),
+            array('title', 'length', 'max'=>100),
             array('title', 'filter', 'filter'=>'strip_tags'),
-			array('content', 'default', 'value'=>NULL),
-			array('date_published', 'date', 'format'=>'YYYY-MM-DD'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, live, title, content, date_updated, date_published, user_id', 'safe', 'on'=>'search'),
-		);
-	}
+            array('content', 'default', 'value'=>NULL),
+            array('date_published', 'date', 'format'=>'YYYY-MM-DD'),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, live, title, content, date_updated, date_published, user_id', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'comments' => array(self::HAS_MANY, 'Comment', 'page_id1'),
-			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
-			'files' => array(self::MANY_MANY, 'File', 'page_has_file(page_id, file_id)'),
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'comments' => array(self::HAS_MANY, 'Comment', 'page_id1'),
+            'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+            'files' => array(self::MANY_MANY, 'File', 'page_has_file(page_id, file_id)'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'live' => 'Live',
-			'title' => 'Title',
-			'content' => 'Content',
-			'date_updated' => 'Date Updated',
-			'date_published' => 'Date Published',
-			'user_id' => 'User',
-		);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'live' => 'Live',
+            'title' => 'Title',
+            'content' => 'Content',
+            'date_updated' => 'Date Updated',
+            'date_published' => 'Date Published',
+            'user_id' => 'User',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria=new CDbCriteria;
 
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('live',$this->live);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('content',$this->content,true);
-		$criteria->compare('date_updated',$this->date_updated,true);
-		$criteria->compare('date_published',$this->date_published,true);
-		$criteria->compare('user_id',$this->user_id,true);
+        $criteria->compare('id',$this->id,true);
+        $criteria->compare('live',$this->live);
+        $criteria->compare('title',$this->title,true);
+        $criteria->compare('content',$this->content,true);
+        $criteria->compare('date_updated',$this->date_updated,true);
+        $criteria->compare('date_published',$this->date_published,true);
+        $criteria->compare('user_id',$this->user_id,true);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Page the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Page the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
     public function onBeforeValidate($event)
     {
@@ -128,6 +128,4 @@ class Page extends CActiveRecord
         }
         parent::onBeforeValidate($event); // TODO: Change the autogenerated stub
     }
-
-
 }
