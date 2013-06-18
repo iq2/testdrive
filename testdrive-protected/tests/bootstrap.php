64f9f6264ef20826604c7c3bf9eb52a4b89d4170
Florian Bacher <florian.bacher@iq2.at>
2013-06-18 14:57:01 +0200

    corrected path to framework

--- testdrive-protected/tests/bootstrap.php
+++ testdrive-protected/tests/bootstrap.php
@@ -5,8 +5,8 @@ $yiit=dirname(__FILE__).'/../../framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 require_once($yiit);
-require_once( Yii::getPathOfAlias('system.test.CTestCase').'.php' ); // Force the PhpUnit autoload via CTestCase file
-require_once( Yii::getPathOfAlias('system.test.CWebTestCase').'.php' ); // Force the PhpUnit autoload via CWebTestCase file
+//require_once( Yii::getPathOfAlias('system.test.CTestCase').'.php' ); // Force the PhpUnit autoload via CTestCase file
+//require_once( Yii::getPathOfAlias('system.test.CWebTestCase').'.php' ); // Force the PhpUnit autoload via CWebTestCase file
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
 Yii::createWebApplication($config);
